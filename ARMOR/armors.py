7190370175c82c59da9c8d4b76c8d52cc65674ae
Coolestwaze <iswazeop@gmail.com>
2024-10-15 16:26:40 -0300

    Update armors.py
    added GOLDEN ARMOR and now there's a function for armors.

--- ARMOR/armors.py
+++ ARMOR/armors.py
@@ -1,19 +1,30 @@
+#Wood armor kit +5% HP
+#Iron armor kit +10% HP
+#Cooper armor kit +15% HP
+#Golden armmor kit +18% HP
 hp = 100
-pW = (hp * 5)/100
-pI = (hp * 10)/100
-pC = (hp * 15)/100
-x = 0
-while x != 1 or x != 2 or x!= 3:
-    x = int(input('Escolha sua armadura: [1 - Armadura de madeira // 2 - Armadura de ferro // 3 - Armadura de cobre]: '))
-    if x == 1:
-        print('Voce escolheu armadura de madeira.')
-        print(f'Seu HP agora: {pW + hp}')
-        exit()
-    elif x == 2:
-        print('Voce escolheu armadura de ferro.')
-        print(f'Seu HP agora: {pI + hp}')
-        exit()
-    elif x == 3:
-        print('Voce escolheu armadura de cobre.')
-        print(F'Seu HP agora: {pC+hp}')
-        exit()
+def armor_set():
+    pw = (hp * 5)/100
+    pi = (hp * 10)/100
+    pc = (hp * 15)/100
+    pg = (hp * 18)/100
+    x = 0
+    while x != 1 or x != 2 or x!= 3:
+        x = int(input('Escolha sua armadura: [1 - Armadura de madeira // 2 - Armadura de ferro // 3 - Armadura de cobre // 4 - Armadura de ouro]: '))
+        if x == 1:
+            print('Voce escolheu armadura de madeira.')
+            print(f'Seu HP agora: {pw + hp}')
+            exit()
+        elif x == 2:
+            print('Voce escolheu armadura de ferro.')
+            print(f'Seu HP agora: {pi + hp}')
+            exit()
+        elif x == 3:
+            print('Voce escolheu armadura de cobre.')
+            print(F'Seu HP agora: {pc+hp}')
+            exit()
+        elif x == 4:
+            print('Voce escolheu armadura de ouro.')
+            print(f'Seu HP agora: {pg + hp}')
+            exit()
+armor_set()
